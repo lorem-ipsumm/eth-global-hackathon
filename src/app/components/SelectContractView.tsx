2758bb970aa83a3fcca8ebd3204625001bc70bfe
Forrest Chew <forrestchew@gmail.com>
2024-04-12 13:27:02 -0700

    feat: adds implementation contract addr input box w/ proxy toggle checkbox. If implementation is selected, the ABI will be fetched from the implementation address but the contract address to call will remain as the proxy

--- src/app/components/SelectContractView.tsx
+++ src/app/components/SelectContractView.tsx
@@ -17,6 +17,7 @@ import {
   FormLabel,
   FormMessage,
 } from "../../components/ui/form";
+import { Checkbox } from "../../components/ui/checkbox";
 import { Input } from "../../components/ui/input";
 import { BeatLoader } from "react-spinners";
 
@@ -31,18 +32,31 @@ const SelectContractView = () => {
     contractAddress: z.string().length(42, {
       message: "Invalid Address | Incorrect length",
     }),
+    contractImplementation: z.string().length(42, {
+      message: "Invalid Address | Incorrect length",
+    }),
+    isProxy: z.boolean(),
   });
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
       contractAddress: "",
+      contractImplementation: "",
+      isProxy: false,
     },
   });
+  const { watch } = form;
+  const isProxy = watch("isProxy");
 
   const onSubmit = async (data: z.infer<typeof FormSchema>) => {
     setIsPending(true);
-    const isContract = await determineContractValidity(data.contractAddress);
+    let isContract;
+    if (!isProxy && !data.contractImplementation) {
+      isContract = await determineContractValidity(data.contractAddress);
+    } else {
+      isContract = await determineContractValidity(data.contractImplementation);
+    }
     setIsPending(false);
     if (!isContract) {
       form.setError("contractAddress", {
@@ -56,11 +70,11 @@ const SelectContractView = () => {
   };
 
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-3">
+    <div className="flex h-full flex-col items-center justify-center gap-2">
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="w-2/3 space-y-6"
+          className="w-3/4 space-y-6"
         >
           <FormField
             control={form.control}
@@ -76,6 +90,36 @@ const SelectContractView = () => {
               </FormItem>
             )}
           />
+          <FormField
+            control={form.control}
+            name="isProxy"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md">
+                <FormLabel>Is Contract a Proxy?</FormLabel>
+                <FormControl>
+                  <Checkbox onCheckedChange={field.onChange} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          {isProxy && (
+            <FormField
+              control={form.control}
+              name="contractImplementation"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Implementation Address</FormLabel>
+                  <FormControl>
+                    <Input placeholder="0x123..." {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Implementation Contract to Fetch ABI
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
           <Button type="submit">
             {isPending ? (
               <BeatLoader size={10} color="white" />
