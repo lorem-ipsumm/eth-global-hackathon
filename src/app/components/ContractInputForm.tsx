1806bbec6c35390cb66051026b502c2541e2494e
Forrest Chew <forrestchew@gmail.com>
2024-04-12 11:57:15 -0700

    feat: create form for input contract address

---
+++ src/app/components/ContractInputForm.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "../../components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../components/ui/form";
+import { Input } from "../../components/ui/input";
+import { useContractAbi } from "../hooks/useContractAbi";
+import { ethers } from "ethers";
+
+const FormSchema = z.object({
+  contractAddress: z
+    .string()
+    .length(42, {
+      message: "Invalid Address | Incorrect length",
+    })
+    .refine((message) => !ethers.isAddress(message), {
+      message: "Invalid Address | Did not pass checksum validation",
+    }),
+});
+
+export const InputForm = () => {
+  const { determineContractValidity } = useContractAbi();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      contractAddress: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof FormSchema>) => {};
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="contractAddress"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contract Address</FormLabel>
+              <FormControl>
+                <Input placeholder="0x123..." {...field} />
+              </FormControl>
+              <FormDescription>Contract to Integrate with</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
