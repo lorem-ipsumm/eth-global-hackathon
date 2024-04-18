26ba1238b1cf70b084155d4c77507ba498667c1c
Forrest Chew <forrestchew@gmail.com>
2024-04-18 16:18:41 -0700

    fix: prevents UI from crashing when an unexpected value is returned from contract call. error will be displayed within label instead.

--- src/app/components/CanvasWidgets/Label.tsx
+++ src/app/components/CanvasWidgets/Label.tsx
@@ -15,13 +15,23 @@ const Label = ({ widgetData }: WIDGET_RENDER_PROPS) => {
     widgetData.externalValue = contractCallReturnData;
   }, [contractCallReturnData]);
 
+  const displayValue = () => {
+    try {
+      if (externalValue !== null) {
+        return externalValue!.toString();
+      }
+    } catch (error: any) {
+      console.error("Error converting externalValue to string:", error);
+      return error.message;
+    }
+    return widgetData.type;
+  };
+
   return (
     <div
       className={`${getDefaultStyles("label")} ${widgetData.styles.length > 0 ? widgetData.styles.join(" ") : null}`}
     >
-      <p>
-        {externalValue !== null ? externalValue!.toString() : widgetData.type}
-      </p>
+      <p>{displayValue()}</p>
     </div>
   );
 };
