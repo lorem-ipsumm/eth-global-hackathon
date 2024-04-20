f4cd09b7f702269784bfe6e516a3918fe574960c
lorem ipsum <lorem.ipsum.crypto@gmail.com>
2024-04-20 14:09:16 -0400

    trigger input change and search on inputs if there is a default value

--- src/app/components/CanvasWidgets/Input.tsx
+++ src/app/components/CanvasWidgets/Input.tsx
@@ -1,12 +1,22 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { WIDGET_RENDER_PROPS } from "~/app/utils.ts/interfaces";
 import { useWidgetStyles } from "~/app/hooks/useWidgetStyles";
 import { useDebounce } from "~/app/hooks/useDebounce";
 import { ContractCallPayloadContext } from "../../Contexts/ContractCallPayloadProvider";
 
 const Input = ({ widgetData }: WIDGET_RENDER_PROPS) => {
-  const [inputValue, setInputValue] = useState<string>(widgetData.defaultValue || "");
+  const [inputValue, setInputValue] = useState<string>(
+    widgetData.defaultValue || "",
+  );
   const { getDefaultStyles } = useWidgetStyles();
+
+  useEffect(() => {
+    // if there is a default value, trigger input change and search
+    if (widgetData.defaultValue) {
+      setInputValue(widgetData.defaultValue);
+      handleSearch(widgetData.defaultValue);
+    }
+  }, []);
 
   const handleSearch = useDebounce((term: any) => {
     setContextValsAndCallContract(term);
