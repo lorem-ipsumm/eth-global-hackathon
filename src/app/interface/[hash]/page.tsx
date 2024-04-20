483c0f746eb7961096da9385e46979f655881bf4
lorem ipsum <lorem.ipsum.crypto@gmail.com>
2024-04-20 10:58:37 -0400

    added loading indicator while fetching interfaces

--- src/app/interface/[hash]/page.tsx
+++ src/app/interface/[hash]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { useAtom } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { BounceLoader, PuffLoader } from "react-spinners";
 import ActionsBar from "~/app/components/ActionsBar";
 import Canvas from "~/app/editor/Canvas";
 import { useWidgetStorage } from "~/app/hooks/useWidgetStorage";
@@ -9,6 +10,7 @@ import { canvasWidgetsAtom } from "~/app/utils.ts/atoms";
 
 const Page = ({ params }: { params: { hash: string } }) => {
   const [, setCanvasWidgets] = useAtom(canvasWidgetsAtom);
+  const [isPending, setIsPending] = useState<boolean>(true);
   const storage = useWidgetStorage();
 
   useEffect(() => {
@@ -20,12 +22,29 @@ const Page = ({ params }: { params: { hash: string } }) => {
     const widgets = await storage.loadWidgetData(params.hash);
     console.log(widgets);
     if (widgets) setCanvasWidgets(widgets);
+    setIsPending(false);
   };
+
+  const renderLoader = () => {
+    if (isPending)
+      return (
+        <div className="flex flex-col gap-3 absolute top-0 left-0 w-full h-full flex items-center justify-center">
+          <BounceLoader
+            size={100}
+            color={"#3b82f6"}
+            className="animate-pulse"
+          />
+          <span className="font-bold text-2xl">Loading Interface</span>
+        </div> 
+      )
+    return null;
+  }
 
   return (
     <div className="h-screen w-screen">
       <ActionsBar/>
       <Canvas />
+      {renderLoader()}
     </div>
   );
 };
