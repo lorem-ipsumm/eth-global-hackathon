36aa64c8c3e7b8e1863ab05f47f5a8344a2e2d3d
lorem ipsum <lorem.ipsum.crypto@gmail.com>
2024-04-20 18:30:22 -0400

    changed fonts, created WalletWrapper, etc.

---
+++ src/app/components/WalletWrapper.tsx
@@ -0,0 +1,48 @@
+"use client";
+import injectedModule from "@web3-onboard/injected-wallets";
+import { init, useConnectWallet } from "@web3-onboard/react";
+import { useAtom } from "jotai";
+import { useEffect } from "react";
+import { walletAtom, onboardAtom } from "../utils.ts/atoms";
+
+const injected = injectedModule();
+const wallets = [injected];
+
+const chains = [
+  {
+    id: 42161,
+    token: "ETH",
+    label: "Arbitrum One",
+    rpcUrl: process.env.NEXT_PUBLIC_ARBITRUM_RPC_URL,
+  },
+];
+
+const onboard = init({
+  apiKey: process.env.NEXT_PUBLIC_ONBOARD_API_KEY,
+  wallets,
+  chains,
+  accountCenter: {
+    desktop: {
+      enabled: false
+    },
+    mobile: {
+      enabled: false
+    }
+  }
+
+});
+
+const WalletWrapper = (props: { children: any }) => {
+  const [, setUserWallet] = useAtom(walletAtom);
+  const [, setOnboard] = useAtom(onboardAtom);
+  const [{ wallet }] = useConnectWallet();
+
+  useEffect(() => {
+    if (wallet) setUserWallet(wallet);
+    setOnboard(onboard);
+  }, [wallet]);
+
+  return props.children;
+};
+
+export default WalletWrapper;
